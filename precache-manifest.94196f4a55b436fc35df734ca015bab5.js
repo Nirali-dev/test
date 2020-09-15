1a0bc63f51509d0e2cfcba5e6f17a7231461c742
Nirali-dev <patoliyanir@gmail.com>
2020-09-15 18:27:19 +0530

    Updates

---
+++ precache-manifest.94196f4a55b436fc35df734ca015bab5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82035d17c9ee3e7b3162a5e0eda800cc",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "3692006a9d4b78377166",
+    "url": "/test/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "015ec8c9c3276a7da36f",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3692006a9d4b78377166",
+    "url": "/test/static/js/2.34ecff67.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test/static/js/2.34ecff67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015ec8c9c3276a7da36f",
+    "url": "/test/static/js/main.12495a71.chunk.js"
+  },
+  {
+    "revision": "0d6dc1693978d1d8b578",
+    "url": "/test/static/js/runtime-main.cce556a0.js"
+  }
+]);
